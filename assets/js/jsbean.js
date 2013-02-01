d736103ce11f4abd2872ae84891462c8297999e7
Vivien Sediri <vsediri@1adev.com>
2013-02-01 14:39:44 +0100

    [Major] New Site using Bootstrap
    Major update with new site using bootstrap and new design

---
+++ assets/js/jsbean.js
@@ -0,0 +1,42 @@
+$(document).ready(function() 
+{
+	var url = window.location;
+	var mySplitResult = url.toString().split("?");
+	var secondValue = mySplitResult[1];
+	if (secondValue === "accordion") 
+	{
+		var thirdValue = mySplitResult[2];
+		if (thirdValue != null) 
+		{
+			thirdValue = parseInt(thirdValue);
+			accordionShowSection(thirdValue);
+		}	
+	}
+});
+
+function showPopup(divId, dialogTitle, widthpx, heightpx)
+{
+	if (widthpx == null)
+	{
+		widthpx = 'auto';
+	}
+	if (heightpx == null)
+	{
+		heightpx = 'auto';
+	}
+	
+	var currentDialog = $( divId ).dialog({
+		resizable: false,
+		height: heightpx,
+		width: widthpx,
+		title: dialogTitle,
+		modal: true,
+		show: "blind",
+		//open: function(event, ui) {$(this).css({'max-height': 700, 'overflow-y': 'auto'});}, 
+	});
+}
+
+function accordionShowSection(section)
+{
+	$( "#accordion" ).accordion("option", "active", section );
+}
--- community/forum/files/forum.js
+++
@@ -1,6 +0,0 @@
-
-// 'stacks' is the Stacks global object.
-// All of the other Stacks related Javascript will 
-// be attatched to it.
-var stacks = {};
-
